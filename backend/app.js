5a034fb31089111ec4127ba5c034c38bd415291e
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2025-01-03 03:42:06 +0530

    Implement eslint for better code quality

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import { createServer } from "http";
-import { graphqlHTTP } from "express-graphql";
-import BookSchema from "./schema/schema.js";
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+import { graphqlHTTP } from 'express-graphql';
+import BookSchema from './schema/schema.js';
 
 const port = process.env.PORT || 8080;
 
@@ -11,14 +11,14 @@ const app = express();
 app.use(cors());  
 app.use(express.json());
 
-app.use("/api", graphqlHTTP({ schema: BookSchema, graphiql : true }));
+app.use('/api', graphqlHTTP({ schema: BookSchema, graphiql : true }));
 
 const server = createServer(app);
 server.listen(port);
 
-server.on("listening", () =>
+server.on('listening', () =>
   console.log(`server is listening on port:${port}`)
 );
-server.on("error", () =>
+server.on('error', () =>
   console.log(`server is not listening on port:${port}`)
 );
